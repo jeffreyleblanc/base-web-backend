265e9bb1032f0bbd8d4e35d0864d85eb9eb6ef94
Jeff LeBlanc <jeffreyleblanc@users.noreply.github.com>
2022-12-12 21:58:50 -0500

    Initial client with better error handling.

---
+++ spool2/client.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python3
+
+# Copyright 2022 Jeffrey LeBlanc
+
+import asyncio
+import signal
+import logging
+import datetime
+import secrets
+import random
+from tornado.websocket import websocket_connect
+from tornado.httpclient import HTTPRequest, HTTPClientError
+
+'''
+https://www.tornadoweb.org/en/stable/websocket.html#tornado.websocket.WebSocketClientConnection
+'''
+
+class SpoolClient:
+
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+        self.conn = None
+
+    async def connect(self):
+        if self.conn is not None:
+            raise Exception("Already connected")
+
+        try:
+            request = HTTPRequest(url=self.url)
+            self.conn = await websocket_connect(url=request,on_message_callback=self.on_message)
+        except HTTPClientError as err:
+            print("err!",err)
+            self.conn = None
+        except ConnectionRefusedError as err:
+            print('refused!!!!',err)
+            self.conn = None
+
+    def close(self):
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
+
+    def on_message(self, message):
+        print("  ==>",message)
+
+    def write_something(self):
+        if self.conn is None:
+            return
+        msg = f"{self.name} {datetime.datetime.now()}"
+        print("<==",msg)
+        self.conn.write_message(msg)
+
+
+async def main():
+    # Set a custom name
+    name = secrets.token_urlsafe(6)
+    url = 'ws://localhost:8898/api/ws/channel/'
+
+    # Make our client and await it connecting
+    client = SpoolClient(name,url)
+    await client.connect()
+
+    # Define a periodic message
+    async def send_something():
+        while True:
+            client.write_something()
+            sleep_for = 2+4*random.random()
+            await asyncio.sleep(sleep_for)
+    asyncio.create_task(send_something())
+
+    # Setup the shutdown systems
+    shutdown_trigger = asyncio.Event()
+    is_shutdown_triggered = False
+    async def exit_handler(signame):
+        nonlocal is_shutdown_triggered
+        if is_shutdown_triggered:
+            logging.info("already shutting down")
+        else:
+            is_shutdown_triggered = True
+            logging.info("shutdown start...")
+            try:
+                client.close()
+            except Exception as e:
+                logging.error(f"Error on shutdown: {e}")
+            logging.info("...shutdown complete")
+            shutdown_trigger.set()
+
+    # Setup signal handlers
+    loop = asyncio.get_event_loop()
+    for signame in ('SIGINT', 'SIGTERM'):
+        loop.add_signal_handler(
+            getattr(signal, signame),
+            lambda signame=signame: asyncio.create_task(exit_handler(signame))
+        )
+
+    # Block on the shutdown trigger
+    await shutdown_trigger.wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ spool2/server.py
@@ -0,0 +1,118 @@
+#! /usr/bin/env python3
+
+# Copyright Jeffrey LeBlanc, 2022. MIT License.
+
+import asyncio
+import datetime
+import signal
+import logging
+import tornado.web
+import tornado.websocket
+import random
+
+class ChannelWebSocket(tornado.websocket.WebSocketHandler):
+
+    def prepare(self):
+        raise tornado.web.HTTPError(403)
+
+    def open(self):
+        self.idx = self.application.register_ws_client(self)
+        print(f'WebSocket {self.idx} on_open')
+        self.write_message("HELLO FROM THE SERVER!")
+
+    def on_message(self, message):
+        self.application.announce(self,message)
+
+    def on_close(self):
+        self.application.unregister_ws_client(self.idx)
+        print(f'WebSocket {self.idx} closed {self}')
+
+
+class MyApp(tornado.web.Application):
+
+    def __init__(self):
+        # Websocket tracking
+        self.ws_client_idx = -1
+        self.ws_clients = {}
+
+        _handlers = [
+            (r"^/api/ws/channel/?$",ChannelWebSocket)
+        ]
+        _settings = {}
+
+        super().__init__(_handlers,**_settings)
+
+    async def on_shutdown(self):
+        logging.info('app::on_shutdown >')
+        for handler in self.ws_clients.values():
+            handler.close()
+        logging.info('< app::on_shutdown')
+
+    def announce(self, sender, message):
+        sender.write_message(f"ECHO: {message}")
+        for wc in self.ws_clients.values():
+            if wc == sender:
+                continue
+            wc.write_message(message)
+
+    async def eject_cycle(self):
+        while True:
+            if len(self.ws_clients) > 0 and random.random()>0.75:
+                pass
+
+    #-- Websocket Tracking ------------------------------------------------#
+
+    def new_ws_client_key(self):
+        ''' Could update to be anything '''
+        self.ws_client_idx += 1
+        return self.ws_client_idx
+
+    def register_ws_client(self, handler):
+        idx = self.new_ws_client_key()
+        self.ws_clients[idx] = handler
+        logging.info('register %s wsclient', idx)
+        return idx
+
+    def unregister_ws_client(self, idx):
+        logging.info('unregister %s wsclient', idx)
+        self.ws_clients.pop(idx)
+
+
+
+async def main():
+
+    # Setup logging
+    logging.basicConfig(level=logging.INFO,format='%(message)s',)
+
+    # Setup the server
+    http_server = MyApp()
+    http_server.listen(8898)
+
+    # Setup the shutdown systems
+    shutdown_trigger = asyncio.Event()
+    is_shutdown_triggered = False
+    async def exit_handler(signame):
+        nonlocal is_shutdown_triggered
+        if not is_shutdown_triggered:
+            is_shutdown_triggered = True
+            try:
+                await http_server.on_shutdown()
+            except Exception as e:
+                logging.error(f"Error on shutdown: {e}")
+            shutdown_trigger.set()
+
+    # Setup signal handlers
+    loop = asyncio.get_event_loop()
+    for signame in ('SIGINT', 'SIGTERM'):
+        loop.add_signal_handler(
+            getattr(signal, signame),
+            lambda signame=signame: asyncio.create_task(exit_handler(signame))
+        )
+
+    # Block on the shutdown trigger
+    await shutdown_trigger.wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
