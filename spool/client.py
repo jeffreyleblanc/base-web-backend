70fe9386eccf7b798a64358313f20e153dc606a8
Jeff LeBlanc <jeffreyleblanc@users.noreply.github.com>
2022-12-12 20:51:26 -0500

    Break out the client into a class.

--- spool/client.py
+++ spool/client.py
@@ -8,30 +8,53 @@ import logging
 import datetime
 import secrets
 import random
-import tornado.websocket
+from tornado.websocket import websocket_connect
 
 '''
 https://www.tornadoweb.org/en/stable/websocket.html#tornado.websocket.WebSocketClientConnection
 '''
 
+class SpoolClient:
+
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+        self.conn = None
+
+    async def connect(self):
+        if self.conn is not None:
+            raise Exception("Already connected")
+        self.conn = await websocket_connect(self.url,on_message_callback=self.on_message)
+
+    def close(self):
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
+
+    def on_message(self, message):
+        print("  ==>",message)
+
+    def write_something(self):
+        msg = f"{self.name} {datetime.datetime.now()}"
+        print("<==",msg)
+        self.conn.write_message(msg)
+
+
 async def main():
     # Set a custom name
     name = secrets.token_urlsafe(6)
-
-    def on_message(message):
-        print("  =>",message)
-
     url = 'ws://localhost:8898/api/ws/channel/'
-    conn = await tornado.websocket.websocket_connect(url,on_message_callback=on_message)
 
+    # Make our client and await it connecting
+    client = SpoolClient(name,url)
+    await client.connect()
+
+    # Define a periodic message
     async def send_something():
         while True:
-            msg = f"{name} {datetime.datetime.now()}"
-            print("<=",msg)
-            conn.write_message(msg)
+            client.write_something()
             sleep_for = 2+4*random.random()
             await asyncio.sleep(sleep_for)
-
     asyncio.create_task(send_something())
 
     # Setup the shutdown systems
@@ -45,7 +68,7 @@ async def main():
             is_shutdown_triggered = True
             logging.info("shutdown start...")
             try:
-                conn.close()
+                client.close()
             except Exception as e:
                 logging.error(f"Error on shutdown: {e}")
             logging.info("...shutdown complete")
